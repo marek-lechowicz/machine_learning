cf69c6d21cb444b100070213cc62438d38d18ff6
Marek Lechowicz <m.lechow13@gmail.com>
2022-04-28 18:57:50 +0200

    lab7 SVM

---
+++ lab7_SVM/get_vocabulary_dict.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import csv
+
+from typing import Dict
+
+
+def get_vocabulary_dict() -> Dict[int, str]:
+    """Read the fixed vocabulary list from the datafile and return.
+
+    :return: a dictionary of words mapped to their indexes
+    """
+
+    # FIXME: Parse data from the 'data/vocab.txt' file.
+    # - The file is saved in tab-separated values (TSV) format.
+    # - Each line contains a word's ID and the word itself.
+    # The output dictionary should map word's ID on the word itself, e.g.:
+    #   {1: 'aa', 2: 'ab', ...}
+
+    # Read the fixed vocabulary list.
+    with open('data\\vocab.txt', 'r') as vocab:
+        
+        # Store all dictionary words in dictionary vocabList.
+        vocabList = {}
+        for line in vocab.readlines():
+            i, word = line.split()
+            vocabList[word] = int(i)
+
+    return vocabList
